ebf5803ad866bac93ab9f618ffb52917b859c29a
stanley ike <stanleyikeh15@gmail.com>
2024-11-23 22:14:57 +0100

    commit all

---
+++ index.js/index.js
@@ -0,0 +1,108 @@
+// let numbers = (1,2,3,4,5)
+// for (let index = 0;index < numbers.length; index++) {
+//     const element = numbers(index);
+//     csole.log(element);
+    
+// }
+// let x=0
+// while (x<=100) {
+//     console.log(x);
+//     x+=2 
+
+// }
+// let randomnumber =Math.ceil(Math.random()* (10 - 5 )+5)
+
+// sole.log(randomnumber);
+
+// myanswer=window.prompt("rock,paper, scissors")
+
+// Array=("rock","paper","scissors")
+
+
+// divs.foreach(Element => {
+
+//     if(Element.classname=="red"){
+//         let newelement = document.createElement("p")
+//         newelement.textContent ="hello stanley";
+//         Element.appendChild(newelement);
+//     }
+//     else{
+//         let newelement = document.createElement("h4")
+//         newelement.textContent = "this is stanley";
+//         Element.appendChild(newelement);
+
+        
+//     }
+
+const people = [{
+    Name:"olu",
+    Image:"https://www.google.com/imgres?q=phones%20images&imgurl=https%3A%2F%2Fwww.trustedreviews.com%2Fwp-content%2Fuploads%2Fsites%2F54%2F2023%2F08%2FBest-smartphone-2.jpg&imgrefurl=https%3A%2F%2Fwww.rockingtalent.com%2F%3Fr%3D192026716&docid=1It_oivV0QRQ-M&tbnid=Vk8HEr5FChDkCM&vet=12ahUKEwimw-avh-2JAxUXZ0EAHaQJM5oQM3oECHUQAA..i&w=1920&h=1080&hcb=2&ved=2ahUKEwimw-avh-2JAxUXZ0EAHaQJM5oQM3oECHUQAA",
+    occupation:"officer",
+},
+
+
+{
+    Name:"alabi",
+    Image:"https://www.google.com/imgres?q=phones%20images&imgurl=https%3A%2F%2Fwww.cnet.com%2Fa%2Fimg%2Fresize%2F9bfa29e3b2803c1f7e76d88c964f17f1d5939480%2Fhub%2F2014%2F05%2F07%2Fe5c8b0aa-6af5-4e8c-a1a9-d6c6233b3c09%2Fpile-of-phones-may-2014-6040-005.jpg%3Fauto%3Dwebp%26width%3D1200&imgrefurl=https%3A%2F%2Fwww.cnet.com%2Fpictures%2Fphones-that-changed-the-world%2F&docid=S8H3z3hzlr5TwM&tbnid=xEEuqk9Kj9ArcM&vet=12ahUKEwimw-avh-2JAxUXZ0EAHaQJM5oQM3oECFYQAA..i&w=1200&h=900&hcb=2&ved=2ahUKEwimw-avh-2JAxUXZ0EAHaQJM5oQM3oECFYQAA",
+    occupation:"sonographer",
+},
+
+
+{
+    Name:"jovit",
+    Image:"https://www.google.com/imgres?q=phones%20images&imgurl=https%3A%2F%2Fmedia.wired.com%2Fphotos%2F6730f701af6d81f801caf818%2Fmaster%2Fw_2560%252Cc_limit%2FOnePlus%25252012R%252520Phone%252520Abstract%252520Background%252520112024%252520SOURCE%252520OnePlus.jpg&imgrefurl=https%3A%2F%2Fwww.wired.com%2Fstory%2Fbest-cheap-phones%2F&docid=291zlY0BYu1GpM&tbnid=M1DtQTT7LVzGtM&vet=12ahUKEwimw-avh-2JAxUXZ0EAHaQJM5oQM3oECHEQAA..i&w=2000&h=1300&hcb=2&ved=2ahUKEwimw-avh-2JAxUXZ0EAHaQJM5oQM3oECHEQAA",
+    occupation:"nurse",
+},
+
+{
+    Name:"tega",
+    Image:"https://www.google.com/imgres?q=phones%20images&imgurl=https%3A%2F%2Fc76c7bbc41.mjedge.net%2Fwp-content%2Fuploads%2Ftc%2F2024%2F07%2Fimage-3.png&imgrefurl=https%3A%2F%2Ftechcabal.com%2F2024%2F07%2F05%2Feight-most-expensive-android-phones-in-2024-luxury-edition%2F&docid=9G8aJhQsoNBbtM&tbnid=DDYPHUA73DWeJM&vet=12ahUKEwimw-avh-2JAxUXZ0EAHaQJM5oQM3oECGYQAA..i&w=728&h=314&hcb=2&ved=2ahUKEwimw-avh-2JAxUXZ0EAHaQJM5oQM3oECGYQAA",
+    occupation:"engineer",
+},
+
+{
+    Name:"stanley",
+    Image:"https://www.google.com/imgres?q=phones%20images&imgurl=https%3A%2F%2Fm.media-amazon.com%2Fimages%2FI%2F71pmwb%2BpahL._AC_SL1500_.jpg&imgrefurl=https%3A%2F%2Fwww.u-buy.com.ng%2Fproduct%2FFSHM3U6FC-dialn-x65-64gb-unlocked-cell-phones-smartphone-6-5-screen-mobile-phone-4500mah-battery-13mp-rear-camera-4g-dual-sim-3-card-slots-android&docid=hf7uqiLQORZxkM&tbnid=Bf3nIlRBqQAenM&vet=12ahUKEwimw-avh-2JAxUXZ0EAHaQJM5oQM3oECBwQAA..i&w=1033&h=1500&hcb=2&ved=2ahUKEwimw-avh-2JAxUXZ0EAHaQJM5oQM3oECBwQAA",
+    occupation:"doctor",
+},
+
+{
+    Name:"phone guru",
+    Image:"https://www.google.com/imgres?q=phones%20images&imgurl=https%3A%2F%2Fwww.stuff.tv%2Fwp-content%2Fuploads%2Fsites%2F2%2F2020%2F10%2FStuff-Affordable-Cheap-Smartphones-LEAD.png&imgrefurl=https%3A%2F%2Fwww.stuff.tv%2Ffeatures%2Fbest-cheap-phones-reviewed%2F&docid=sNHYu3W2XCJA_M&tbnid=wcerq8MOCd6hvM&vet=12ahUKEwimw-avh-2JAxUXZ0EAHaQJM5oQM3oECDkQAA..i&w=1500&h=1000&hcb=2&ved=2ahUKEwimw-avh-2JAxUXZ0EAHaQJM5oQM3oECDkQAA",
+    occupation:"itel",
+},
+
+{
+    Name:"star",
+    Image:"https://www.google.com/imgres?q=phones%20images&imgurl=https%3A%2F%2Fimages.ctfassets.net%2Fwcfotm6rrl7u%2F4WfMrZhWkEWsX36UbgH0NQ%2F3a62cc8fbc985307fb15e817bd88a9e3%2FIN-Nokia_G42_5G-BlackSipper-Hero-Phones.png%3Fw%3D1920%26fm%3Dwebp%26q%3D80&imgrefurl=https%3A%2F%2Fwww.hmd.com%2Fen_in&docid=yghDodah5Na63M&tbnid=lwexFEqEYMMNVM&vet=12ahUKEwimw-avh-2JAxUXZ0EAHaQJM5oQM3oECEUQAA..i&w=1920&h=2976&hcb=2&itg=1&ved=2ahUKEwimw-avh-2JAxUXZ0EAHaQJM5oQM3oECEUQAA",
+    occupation:"faster",
+},
+
+{
+    Name: "iphone",
+    Image: "https://www.google.com/imgres?q=phones%20images&imgurl=https%3A%2F%2Fi.pcmag.com%2Fimagery%2Froundups%2F01WOu4NbEnv3pJ54qp7j50k-16..v1647874749.jpg&imgrefurl=https%3A%2F%2Fwww.pcmag.com%2Fpicks%2Fthe-best-cheap-phones&docid=gyZSni8IqE9X_M&tbnid=YMCTGHGBQw7AVM&vet=12ahUKEwimw-avh-2JAxUXZ0EAHaQJM5oQM3oECDcQAA..i&w=1028&h=578&hcb=2&ved=2ahUKEwimw-avh-2JAxUXZ0EAHaQJM5oQM3oECDcQAA",
+    occupation: "phone seller",
+},
+]
+
+let body = document.getelementbyid("body")
+console.log("hello");
+
+
+people.foreach(person => {
+    let card = document.createElement("div")
+    card.classList.add("card")
+    body.appendChild(card)
+
+    let image = document.createElement("img")
+    image.src =person.Image
+    card.appendChild(child)
+
+    let name = document.createElement(h2)
+    name.textContent = person.Name.appendChild(name)
+
+    let occupation = document.createElement("p")
+    occupation.textContent = person.occupation
+    card.appendChild(occupation)
+})
+
